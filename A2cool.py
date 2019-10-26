fba8af8ea345bf092f7c73014e654d5883ddc0c7
Moardant <liam.scott.russell@gmail.com>
2019-10-27 10:32:27 +1300

    Adds the Deque class (from the main assignment 2 file)

--- A2cool.py
+++ A2cool.py
@@ -2,6 +2,84 @@ from screen import Screen, Message, Card, get_current_screen_size
 from time import sleep
 
 
+class Deque:
+    """
+    Implements a deque class
+
+    The front of the deque is stored in the last element of the list.
+    The rear of the deque is stored in the first element of the list.
+
+    Operations:
+
+        add_front(item)
+        add_read(item)
+        remove_front()
+        remove_rear()
+        size()
+        peek()
+        peeklast()
+        printall()
+    """
+
+    def __init__(self):
+        # sets up the python list that holds the deque items
+        self.items = []
+
+    def add_front(self, item):
+        # adds the item to the front of the deque, i.e. append
+        self.items.append(item)
+
+    def add_rear(self, item):
+        # adds the item to the end of the deque, i.e. prepend
+        self.items = [item] + self.items
+
+    def remove_front(self):
+        # removes and returns the item in the front of the deque
+        if self.size() == 0:
+            return None  # no data in deque
+        else:
+            return self.items.pop()
+
+    def remove_rear(self):
+        # removes and returns the item at the rear of the deque
+        if self.size() == 0:
+            return None  # no data in deque
+        else:
+            temp = self.items[0]
+            self.items = self.items[1:]  # removes the first item
+            return temp
+
+    def size(self):
+        # returns the size of the deque
+        return len(self.items)
+
+    def peek(self):
+        # returns the item at the front of the deque, without removing it
+        if self.size() == 0:
+            return None  # no data in deque
+        else:
+            return self.items[-1]
+
+    def peeklast(self):
+        # returns the item at the rear of the deque, without removing it
+        if self.size() == 0:
+            return None  # no data in deque
+        else:
+            return self.items[0]
+
+    def printall(self, index):
+        # prints the data stored within the deque
+        if self.size() == 0:
+            print(' ')
+        elif index == 0:
+            # prints the first item, followed by '*' characters
+            print(str(self.items[0]), ' *' * (len(self.items) - 1), sep="")
+        else:
+            # necessary to use the list comprehension,
+            # as string objects are expected
+            print(" ".join([str(x) for x in self.items]))
+
+
 def main():
     try:
         rows, cols = get_current_screen_size()
@@ -23,7 +101,7 @@ def main():
         message = Message(disp_string, s)
         message.display_centre()  # displays the message to the user
         s.display()
-        sleep(2)
+        sleep(2) # Wait a little bit
         rows, cols = get_current_screen_size()
     else:
         s.clear()
