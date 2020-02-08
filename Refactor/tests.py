2c495bf7700686c901fa41518d6bce25145dcc24
Moardant <liam.scott.russell@gmail.com>
2020-02-08 13:54:21 +1300

    Add ability to set the points on a screen, along with tests for this

--- Refactor/tests.py
+++ Refactor/tests.py
@@ -21,6 +21,25 @@ class TestScreen(unittest.TestCase):
         with self.assertRaises(IndexError):
             test_screen.get_point(-1, 23)
 
+    def test_set_point_with_valid_coordinates(self):
+        """
+        Test that a point on the screen can be set with a value
+        """
+        test_screen = Screen(2, 2)
+        test_screen.set_point(1, 1, "#")
+
+        expected_point = "#"
+        actual_point = test_screen.get_point(1, 1)
+
+        self.assertEqual(expected_point, actual_point)
+
+    def test_set_point_with_invalid_coordinates(self):
+        """
+        Test that an invalid coordinate won't change the state of the screen
+        """
+        test_screen = Screen(2, 2)
+        with self.assertRaises(IndexError):
+            test_screen.set_point(1, 2, "#")
 
 if __name__ == "__main__":
     unittest.main()
--- new_screen.py
+++ new_screen.py
@@ -12,3 +12,13 @@ class Screen:
             return self.__matrix[x][y]
         else:
             raise IndexError("Coordinates invalid")
+
+    def set_point(self, x, y, value):
+        x_is_valid = x >= 0 and x < self.__cols
+        y_is_valid = y >= 0 and y < self.__rows
+
+        if x_is_valid and y_is_valid:
+            self.__matrix[x][y] = value
+        else:
+            raise IndexError("Coordinates invalid")
+
