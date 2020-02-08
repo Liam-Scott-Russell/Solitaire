8c12a6bc44e1ea51a5aa711cbb7cb4f574938502
Moardant <liam.scott.russell@gmail.com>
2020-02-08 17:09:44 +1300

    Add tests for drawing vertical lines on the screen

--- ScreenHelper.py
+++ ScreenHelper.py
@@ -1,7 +1,7 @@
 class ScreenHelper:
     @staticmethod
     def draw_line(screen, x1, y1, x2, y2, value):
-        if screen.point_is_valid(x1, y1) and screen.point_is_valid(x2+1, y2+1):
+        if screen.point_is_valid(x1, y1) and screen.point_is_valid(x2, y2):
             for row in range(x1, x2+1):
                 for col in range(y1, y2+1):
                     screen.set_point(row, col, value)
--- Tests/test_ScreenHelper.py
+++ Tests/test_ScreenHelper.py
@@ -25,6 +25,31 @@ class TestScreenHelper(TestCase):
         screen = Screen(2, 2)
 
         with self.assertRaises(IndexError):
+            ScreenHelper.draw_line(screen, 0, 0, 3, 0, "#")
+
+        self.assertListEqual(screen.get_matrix(), Screen(2, 2).get_matrix())
+
+    def test_draw_line_vertically_with_valid_coordinates(self):
+        """
+        A test that a vertical line can be drawn
+        """
+        screen = Screen(4, 4)
+
+        ScreenHelper.draw_line(screen, 1, 0, 2, 0, "#")
+
+        expected_screen = Screen(4, 4)
+        expected_screen.set_point(1, 0, "#")
+        expected_screen.set_point(2, 0, "#")
+
+        self.assertListEqual(screen.get_matrix(), expected_screen.get_matrix())
+
+    def test_draw_line_vertical_with_invalid_coordinates(self):
+        """
+        A test that a vertical line won't be drawn for invalid coordinates
+        """
+        screen = Screen(2, 2)
+
+        with self.assertRaises(IndexError):
             ScreenHelper.draw_line(screen, 0, 0, 0, 3, "#")
 
         self.assertListEqual(screen.get_matrix(), Screen(2, 2).get_matrix())
