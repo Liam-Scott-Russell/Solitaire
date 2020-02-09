95dadad5488bf7c1ba957122da8ba308aed44df5
Moardant <liam.scott.russell@gmail.com>
2020-02-09 14:06:12 +1300

    Change the line drawing method to use Bresenham's algorithm to get the points

--- ScreenHelper.py
+++ ScreenHelper.py
@@ -1,9 +1,13 @@
+from MathHelper import MathHelper
+
+
 class ScreenHelper:
     @staticmethod
     def draw_line(screen, x1, y1, x2, y2, value):
         if screen.point_is_valid(x1, y1) and screen.point_is_valid(x2, y2):
-            for x_value in range(x1, x2 + 1):
-                for y_value in range(y1, y2 + 1):
-                    screen.set_point(x_value, y_value, value)
+            points_to_draw = MathHelper.calculate_points_on_line(x1, y1, x2, y2)
+
+            for point in points_to_draw:
+                screen.set_point(point[0], point[1], value)
         else:
             raise IndexError("Coordinates not valid")
--- Tests/test_ScreenHelper.py
+++ Tests/test_ScreenHelper.py
@@ -53,3 +53,28 @@ class TestScreenHelper(TestCase):
             ScreenHelper.draw_line(screen, 0, 0, 0, 3, "#")
 
         self.assertListEqual(screen.get_matrix(), Screen(2, 2).get_matrix())
+
+    def test_draw_line_diagonally_with_valid_coordinates(self):
+        """
+        A test that a diagonal line can be drawn
+        """
+        screen = Screen(4, 4)
+
+        ScreenHelper.draw_line(screen, 1, 1, 2, 2, "#")
+
+        expected_screen = Screen(4, 4)
+        expected_screen.set_point(1, 1, "#")
+        expected_screen.set_point(2, 2, "#")
+
+        self.assertListEqual(screen.get_matrix(), expected_screen.get_matrix())
+
+    def test_draw_line_diagonally_with_invalid_coordinates(self):
+        """
+        A test that a diagonal line won't be drawn for invalid coordinates
+        """
+        screen = Screen(2, 2)
+
+        with self.assertRaises(IndexError):
+            ScreenHelper.draw_line(screen, 0, 0, 0, 3, "#")
+
+        self.assertListEqual(screen.get_matrix(), Screen(2, 2).get_matrix())
