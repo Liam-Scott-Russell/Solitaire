6830c903c2d72d6b874b04c7a06f3736da71ccd4
Moardant <liam.scott.russell@gmail.com>
2020-02-15 12:42:03 +1300

    Add Card Class and tests

---
+++ Card.py
@@ -0,0 +1,16 @@
+class Card:
+    def __init__(self, number):
+        self.number = number
+
+    def get_representation(self, show_number=True):
+        """
+        Cards look like this:
+         ____
+        |   2|
+        |    |
+        |    |
+        |____|
+        """
+        character_to_show = str(self.number) if show_number else "*"
+        representation = " ____\n|  {:>2}|\n|    |\n|    |\n|____|".format(character_to_show)
+        return representation
---
+++ Tests/test_Card.py
@@ -0,0 +1,51 @@
+from unittest import TestCase
+from Card import Card
+
+class TestCard(TestCase):
+    def test_get_representation_with_single_digit_and_number_shown(self):
+        test_card = Card(2)
+        actual_representation = test_card.get_representation()
+        expected_representation = ""
+        expected_representation += " ____\n"
+        expected_representation += "|   2|\n"
+        expected_representation += "|    |\n"
+        expected_representation += "|    |\n"
+        expected_representation += "|____|"
+
+        self.assertEqual(expected_representation, actual_representation)
+
+    def test_get_representation_with_double_digit_and_number_shown(self):
+        test_card = Card(10)
+        actual_representation = test_card.get_representation()
+        expected_representation = ""
+        expected_representation += " ____\n"
+        expected_representation += "|  10|\n"
+        expected_representation += "|    |\n"
+        expected_representation += "|    |\n"
+        expected_representation += "|____|"
+
+        self.assertEqual(expected_representation, actual_representation)
+
+    def test_get_representation_with_single_digit_and_number_not_shown(self):
+        test_card = Card(2)
+        actual_representation = test_card.get_representation(show_number=False)
+        expected_representation = ""
+        expected_representation += " ____\n"
+        expected_representation += "|   *|\n"
+        expected_representation += "|    |\n"
+        expected_representation += "|    |\n"
+        expected_representation += "|____|"
+
+        self.assertEqual(expected_representation, actual_representation)
+
+    def test_get_representation_with_double_digit_and_number_not_shown(self):
+        test_card = Card(10)
+        actual_representation = test_card.get_representation(show_number=False)
+        expected_representation = ""
+        expected_representation += " ____\n"
+        expected_representation += "|   *|\n"
+        expected_representation += "|    |\n"
+        expected_representation += "|    |\n"
+        expected_representation += "|____|"
+
+        self.assertEqual(expected_representation, actual_representation)
