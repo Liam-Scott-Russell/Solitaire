cf414334a490f89e45ea9a32292dc9c0e7ccc21c
Moardant <liam.scott.russell@gmail.com>
2020-02-15 19:34:25 +1300

    Add ability to check move condition 2, with tests

--- MoveChecker.py
+++ MoveChecker.py
@@ -1,12 +1,14 @@
 class MoveChecker:
     @staticmethod
     def check_move(column1, column2, gamestate):
-        column1 = str(column1)
-        column2 = str(column2)
-        should_check_condition_1 = column1 == "0" and column2 == "0"
+
+        should_check_condition_1 = column1 == 0 and column2 == 0
+        should_check_condition_2 = column1 == 0 and column2 > 0
 
         if should_check_condition_1:
             return MoveChecker.check_condition_1(gamestate)
+        elif should_check_condition_2:
+            return MoveChecker.check_condition_2(gamestate, column2)
         else:
             return False
 
@@ -14,3 +16,20 @@ class MoveChecker:
     def check_condition_1(gamestate):
         is_items_in_first_column = gamestate.columns[0].size() != 0
         return is_items_in_first_column
+
+    @staticmethod
+    def check_condition_2(gamestate, column2):
+        first_card_deque = gamestate.columns[0]
+        second_card_deque = gamestate.columns[column2]
+
+        is_items_in_first_column = first_card_deque.size() != 0
+        is_items_in_second_column = second_card_deque.size() != 0
+
+        if is_items_in_first_column and is_items_in_second_column:
+            first_card_in_second_column = second_card_deque.peek_front()
+            first_card_in_first_column = first_card_deque.peek_front()
+
+            first_card_is_one_less_than_second = first_card_in_first_column.number == first_card_in_second_column.number - 1
+            return first_card_is_one_less_than_second
+        else:
+            return is_items_in_first_column
--- Tests/test_MoveChecker.py
+++ Tests/test_MoveChecker.py
@@ -2,12 +2,13 @@ from unittest import TestCase
 from MoveChecker import *
 from Deque import Deque
 from GameState import GameState
+from Card import Card
 
 
 class TestMoveChecker(TestCase):
     def test_check_move_with_condition_1_and_items_in_deque(self):
         game = GameState(2)
-        game.columns[0].items = [1, 2]
+        game.columns[0].items = [Card(1), Card(2)]
 
         self.assertTrue(MoveChecker.check_move(0, 0, game))
 
@@ -15,3 +16,28 @@ class TestMoveChecker(TestCase):
         game = GameState(2)
 
         self.assertFalse(MoveChecker.check_move(0, 0, game))
+
+    def test_check_move_with_condition_2_and_valid_items_in_destination(self):
+        game = GameState(2)
+        game.columns[0].items = [Card(1), Card(2)]
+        game.columns[1].items = [Card(4), Card(3)]
+
+        self.assertTrue(MoveChecker.check_move(0, 1, game))
+
+    def test_check_move_with_condition_2_and_invalid_items_in_destination(self):
+        game = GameState(2)
+        game.columns[0].items = [Card(1), Card(2)]
+        game.columns[1].items = [Card(5), Card(4)]
+
+        self.assertFalse(MoveChecker.check_move(0, 1, game))
+
+    def test_check_move_with_condition_2_and_no_items_in_destination(self):
+        game = GameState(2)
+        game.columns[0].items = [Card(1), Card(2)]
+
+        self.assertTrue(MoveChecker.check_move(0, 1, game))
+
+    def test_check_move_with_condition_2_and_no_items_in_destination_and_no_items_in_source(self):
+        game = GameState(2)
+
+        self.assertFalse(MoveChecker.check_move(0, 1, game))
