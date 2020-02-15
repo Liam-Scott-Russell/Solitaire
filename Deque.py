5c7f68991ab590031c1ac28372acb36c551314b7
Moardant <liam.scott.russell@gmail.com>
2020-02-15 16:19:22 +1300

    Add Deque class, with tests

---
+++ Deque.py
@@ -0,0 +1,38 @@
+class Deque:
+    def __init__(self):
+        self.items = []
+
+    def add_front(self, item):
+        self.items.append(item)
+
+    def add_rear(self, item):
+        self.items = [item] + self.items
+
+    def remove_front(self):
+        if self.size() == 0:
+            raise IndexError("No items in Deque")
+        else:
+            return self.items.pop()
+
+    def remove_rear(self):
+        if self.size() == 0:
+            raise IndexError("No items in Deque")
+        else:
+            temp = self.items[0]
+            self.items = self.items[1:]
+            return temp
+
+    def size(self):
+        return len(self.items)
+
+    def peek_front(self):
+        if self.size() == 0:
+            raise IndexError("No items in Deque")
+        else:
+            return self.items[-1]
+
+    def peek_rear(self):
+        if self.size() == 0:
+            raise IndexError("No items in Deque")
+        else:
+            return self.items[0]
---
+++ Tests/test_Deque.py
@@ -0,0 +1,92 @@
+from unittest import TestCase
+from Deque import Deque
+
+
+class TestDeque(TestCase):
+    def test_add_front_and_rear(self):
+        deque = Deque()
+        deque.add_front(0)
+        deque.add_rear(1)
+        deque.add_front(2)
+        deque.add_rear(3)
+
+        actual_items = deque.items
+        expected_items = [3, 1, 0, 2]
+
+        self.assertListEqual(actual_items, expected_items)
+
+    def test_remove_front_with_items_in_deque(self):
+        deque = Deque()
+        deque.add_front(0)
+        deque.add_front(1)
+        deque.add_front(2)
+        deque.add_front(3)
+
+        actual_front = deque.remove_front()
+        expected_front = 3
+
+        self.assertEqual(actual_front, expected_front)
+        self.assertListEqual(deque.items, [0, 1, 2])
+
+    def test_remove_front_without_items_in_deque(self):
+        deque = Deque()
+
+        with self.assertRaises(IndexError):
+            deque.remove_front()
+
+    def test_remove_rear_with_items_in_deque(self):
+        deque = Deque()
+        deque.add_front(0)
+        deque.add_front(1)
+        deque.add_front(2)
+        deque.add_front(3)
+
+        actual_rear = deque.remove_rear()
+        expected_front = 0
+
+        self.assertEqual(actual_rear, expected_front)
+        self.assertListEqual(deque.items, [1, 2, 3])
+
+    def test_remove_rear_without_items_in_deque(self):
+        deque = Deque()
+
+        with self.assertRaises(IndexError):
+            deque.remove_rear()
+
+    def test_peek_front_with_items_in_deque(self):
+        deque = Deque()
+        deque.add_front(0)
+        deque.add_front(1)
+        deque.add_front(2)
+        deque.add_front(3)
+
+        actual_front = deque.peek_front()
+        expected_front = 3
+
+        self.assertEqual(actual_front, expected_front)
+        self.assertListEqual(deque.items, [0, 1, 2, 3])
+
+    def test_peek_front_without_items_in_deque(self):
+        deque = Deque()
+
+        with self.assertRaises(IndexError):
+            deque.peek_front()
+
+    def test_peek_rear(self):
+        deque = Deque()
+        deque.add_front(0)
+        deque.add_front(1)
+        deque.add_front(2)
+        deque.add_front(3)
+
+        actual_rear = deque.peek_rear()
+        expected_rear = 0
+
+        self.assertEqual(actual_rear, expected_rear)
+        self.assertListEqual(deque.items, [0, 1, 2, 3])
+
+    def test_peek_rear_without_items_in_deque(self):
+        deque = Deque()
+
+        with self.assertRaises(IndexError):
+            deque.peek_rear()
